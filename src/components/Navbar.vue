308102964a9bff195d66295266721e458091400c
AndreaMargutti <marguttiandrea30@gmail.com>
2024-12-03 10:19:02 +0100

    add logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,13 +9,30 @@ export default {
 </script>
 
 <template>
-    <nav class="d-flex align-items-center">
-        <RouterLink to="/">Home</RouterLink>
+    <nav class="d-flex align-items-center justify-content-between p-3">
+        <div>
+            <img src="../../public/Food-Delivery-Service-PNG-High-Quality-Image.png" alt="logo">
+        </div>
+        <div id="nav-link">
+            <RouterLink to="/">Home</RouterLink>
+            <a href="#">Accedi al tuo Ristorante</a>
+        </div>
     </nav>
 </template>
 
 <style scoped lang="scss">
 nav {
     min-height: 2.5rem;
+    background-color: #4fae5a;
+    img {
+        height: 5.5rem;
+    }
 }
+
+#nav-link {
+    * {
+        margin-right: 10px;
+    }
+}
+
 </style>
