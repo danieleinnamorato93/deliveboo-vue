36bbbb436d234642b187d8fc42432dc4d84af990
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 15:44:04 +0100

    Rf4-imported store in typelist

--- src/components/TypesList.vue
+++ src/components/TypesList.vue
@@ -1,61 +1,37 @@
 <script>
+import { store } from '../store';
 export default {
   data() {
-    return {};
+    return {
+      store,
+    };
   },
 };
 </script>
 
 <template>
-  <div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="..." />
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="..." />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img src="..." class="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExample"
-      data-bs-slide="prev"
-    >
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExample"
-      data-bs-slide="next"
-    >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+
   </div>
 </template>
 
-<style lang="scss" scoped>
-.card {
-  width: 200px;
-}
-
-h4 {
-  margin-top: 10px;
-  color: #ffa62b;
-}
-
-figure {
-  padding: 10px;
-  text-align: center;
-  margin-bottom: 0;
-  img {
-    height: 100px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
