2ed56794d474b6fd6f6bb4cd68d2b1b8e747a113
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-09 15:18:48 +0100

    carousel

--- src/components/TypesList.vue
+++ src/components/TypesList.vue
@@ -7,59 +7,36 @@ export default {
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img
-              src="../../public/pngtree-hot-cheesy-pizza-isolated-on-transparent-background-png-image_9133345.png"
-            />
-            <h4>Pizza</h4>
-          </figure>
-        </div>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="..." />
       </div>
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
-            <h4>Sushi</h4>
-          </figure>
-        </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
       </div>
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img src="../../public/kebab-with-ai-generated-free-png.webp" />
-            <h4>Kebab</h4>
-          </figure>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
-            <h4>Sushi</h4>
-          </figure>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
-            <h4>Sushi</h4>
-          </figure>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <figure>
-            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
-            <h4>Sushi</h4>
-          </figure>
-        </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
       </div>
     </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
 </template>
 
