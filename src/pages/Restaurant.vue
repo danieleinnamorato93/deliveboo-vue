b6fa2fc432726264942eb3476aeef48dcb297df8
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 15:57:49 +0100

    RF4- Component RestaurantCard in Page/Restaurant

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,15 +1,23 @@
 <script>
+import RestaurantCard from '../components/RestaurantCard.vue';
+import RestaurantCard from '../components/RestaurantCard.vue';
+
 export default {
+  
   data() {
     return {
       
     }
+  },
+  component: {
+    RestaurantCard
   }
 }
 </script>
 
 <template>
 <h1>Sono un singolo ristorante</h1>
+<RestaurantCard />
 </template>
 
 <style scoped lang="scss">
