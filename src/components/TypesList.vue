18f23f9d4ae7f3ba7e596b8854cae29ffdfaae96
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 17:29:01 +0100

    RF3-added a toggle to handle typeid

--- src/components/TypesList.vue
+++ src/components/TypesList.vue
@@ -7,7 +7,17 @@ export default {
     }
   },
   methods: {
-    
+    getTypeId(typeId) {
+      // cerca indice typeid
+      const index = store.clickedTypes.indexOf(typeId);
+      // Se non è presente, aggiungilo
+      if (index === -1) {
+        store.clickedTypes.push(typeId);
+      } else {
+        // Se è presente, rimuovilo
+        store.clickedTypes.splice(index, 1);
+      }
+    },
   },
 }
 </script>
@@ -19,14 +29,16 @@ export default {
         <div :id="'carousel-' + type.id" class="carousel slide">
           <div class="carousel-inner">
             <div class="carousel-item" :class="{ active: index === 0 }">
-              <img src="..." class="d-block w-100" alt="..." />
+              <img src="..." class="d-block w-100" alt="..." @click="getTypeId(type.id)" />
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="'#carousel-' + type.id" data-bs-slide="prev">
+          <button class="carousel-control-prev" type="button" data-bs-target="'#carousel-' + type.id"
+            data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="'#carousel-' + type.id" data-bs-slide="next">
+          <button class="carousel-control-next" type="button" data-bs-target="'#carousel-' + type.id"
+            data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
