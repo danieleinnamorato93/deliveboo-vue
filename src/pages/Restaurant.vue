be947203b9ab024ee88e5ac1c02904666f29ced4
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 16:24:36 +0100

    RF4 - Store in page/Restaurant.vue

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,22 +1,30 @@
 <script>
 import RestaurantCard from '../components/RestaurantCard.vue';
+import {store} from '../js/store.js';
 
 export default {
-  
+  name:"Restaurant",
   data() {
     return {
-      
+     
     }
   },
   component: {
     RestaurantCard
+  },
+  computed: {
+    restaurantsList() {
+      return store.restaurantsList;
+    }
   }
 }
 </script>
 
 <template>
 <h1>Sono un singolo ristorante</h1>
+
 <RestaurantCard />
+
 </template>
 
 <style scoped lang="scss">
