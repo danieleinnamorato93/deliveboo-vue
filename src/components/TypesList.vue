462e4028b73116a1da44014b6b9e1d418380fd9d
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 16:09:47 +0100

    RF3-modified carousel

--- src/components/TypesList.vue
+++ src/components/TypesList.vue
@@ -19,34 +19,18 @@ export default {
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
-        <div
-          id="carouselExample"
-          class="carousel slide"
-          v-for="type in store.TypesList"
-          :key="type.id"
-          @click="showSingleType(type.id)"
-        >
+      <div class="col-12" v-for="type in store.TypesList" :key="type.id">
+        <div :id="'carousel-' + type.id" class="carousel slide">
           <div class="carousel-inner">
-            <div class="carousel-item">
+            <div class="carousel-item" :class="{ active: index === 0 }" @click="showSingleType(type.id)">
               <img src="..." class="d-block w-100" alt="..." />
             </div>
           </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="prev"
-          >
+          <button class="carousel-control-prev" type="button" data-bs-target="'#carousel-' + type.id" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="next"
-          >
+          <button class="carousel-control-next" type="button" data-bs-target="'#carousel-' + type.id" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
