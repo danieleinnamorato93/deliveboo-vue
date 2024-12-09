10df48a656da170cdf3fd5fd60b37cb92570cbad
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 15:43:43 +0100

    RF4- adeed router list show for type

--- src/js/router.js
+++ src/js/router.js
@@ -1,11 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../pages/Home.vue";
 import Checkout from "../pages/Checkout.vue";
+import TypesList from "../components/TypesList.vue";
 
 const routes = [
   //{ path: "/about", component: AboutView },
   { path: '/', component: Home },
-  { path: '/checkout', component: Checkout }
+  { path: '/checkout', component: Checkout },
+  { path: '/types/:id', component: TypesList, name:'types.show' },
+
 ];
 
 const router = createRouter({
