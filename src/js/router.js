6ccb1a1312f9002bfcad1b58ad3165b0266fe544
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 15:31:44 +0100

    RF4 - route to the restaurant page

--- src/js/router.js
+++ src/js/router.js
@@ -1,11 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../pages/Home.vue";
 import Checkout from "../pages/Checkout.vue";
+import Restaurant from "../pages/Restaurant.vue";
 
 const routes = [
   //{ path: "/about", component: AboutView },
   { path: '/', component: Home },
-  { path: '/checkout', component: Checkout }
+  { path: '/checkout', component: Checkout },
+  { path: '/restaurant', component: Restaurant },
 ];
 
 const router = createRouter({
