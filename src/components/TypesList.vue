409a03f42880357627da7f32a7492d60efad2eee
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-09 12:18:30 +0100

    error fixed

--- src/components/TypesList.vue
+++ src/components/TypesList.vue
@@ -1,87 +1,84 @@
-Type.vue
-
 <script>
 export default {
   data() {
-    return {
-      
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/pngtree-hot-cheesy-pizza-isolated-on-transparent-background-png-image_9133345.png"/>
-                  <h4>Pizza</h4>
-                </figure>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/sushi-with-ai-generated-free-png.webp"/>
-                  <h4>Sushi</h4>
-                </figure>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/kebab-with-ai-generated-free-png.webp"/>
-                  <h4>Kebab</h4>
-                </figure>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/sushi-with-ai-generated-free-png.webp"/>
-                  <h4>Sushi</h4>
-                </figure>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/sushi-with-ai-generated-free-png.webp"/>
-                  <h4>Sushi</h4>
-                </figure>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <figure>
-                  <img src="../../public/sushi-with-ai-generated-free-png.webp"/>
-                  <h4>Sushi</h4>
-                </figure>
-              </div>
-            </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img
+              src="../../public/pngtree-hot-cheesy-pizza-isolated-on-transparent-background-png-image_9133345.png"
+            />
+            <h4>Pizza</h4>
+          </figure>
         </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
+            <h4>Sushi</h4>
+          </figure>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img src="../../public/kebab-with-ai-generated-free-png.webp" />
+            <h4>Kebab</h4>
+          </figure>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
+            <h4>Sushi</h4>
+          </figure>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
+            <h4>Sushi</h4>
+          </figure>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <figure>
+            <img src="../../public/sushi-with-ai-generated-free-png.webp" />
+            <h4>Sushi</h4>
+          </figure>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
 .card {
-    width: 200px;
+  width: 200px;
 }
 
 h4 {
-    margin-top: 10px;
-    color: #FFA62B;
+  margin-top: 10px;
+  color: #ffa62b;
 }
 
 figure {
-    padding: 10px;
-    text-align: center;
-    margin-bottom: 0;
-    img{
-        height: 100px;
-    }
+  padding: 10px;
+  text-align: center;
+  margin-bottom: 0;
+  img {
+    height: 100px;
+  }
 }
 </style>
