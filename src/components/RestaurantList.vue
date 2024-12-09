35ced868e99d0ef347898eea253e5c5b0c41fa86
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-09 12:47:05 +0100

    added methos for axios call

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -1,18 +1,46 @@
 <script>
 import RestaurantCard from "./RestaurantCard.vue";
 import axios from "axios";
+import {store} from '../js/store.js'
 
 export default {
   data() {
     return {
- 
+      store,
+      apiRestaurants:'',
+      apiTypes:'',
     };
   },
 
   components: {
     RestaurantCard,
   },
+  
+  methods: {
+          //chiamata principale per ottenere la lista dei ristoranti
+    getRestaurants (){
+      axios.get(this.apiRestaurants)
+        .then((response) => {
+        store.restaurantsList = response.data.data;
+        })
+      },
+
+      // chiamata per ottenere le tipologie dei ristoranti
+    getTypes (){
+      axios.get(this.apiTypes)
+        .then((response)=> {
+        store.typesList = response.data;
+        })
+      },
+  },
+
+  created(){
+    this.getRestaurants();
+    this.getTypes();
+  },
+
 };
+
 </script>
 
 <template>
