71b57d6f6fbc4466750cba227651bfe5cd6b2a9c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 16:39:58 +0100

    RF4 -Computed and v-bind in restaurantObject

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,5 +1,6 @@
 <script>
 import RestaurantCard from '../components/RestaurantCard.vue';
+import RestaurantCard from '../components/RestaurantCard.vue';
 import {store} from '../js/store.js';
 
 export default {
@@ -16,15 +17,17 @@ export default {
     restaurantsList() {
       return store.restaurantsList;
     }
-  }
+  },
 }
 </script>
 
 <template>
 <h1>Sono un singolo ristorante</h1>
 
-<RestaurantCard />
-
+    <div v-for="restaurant in restaurantsList" :key="restaurant.id">
+      <RestaurantCard :restaurantObject="restaurant" />
+    </div>
+  
 </template>
 
 <style scoped lang="scss">
