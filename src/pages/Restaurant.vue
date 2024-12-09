6823eba435d45f005eae0e69ba15faef025214a7
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 19:55:36 +0100

    RF4 - Axios for restaurant.ID

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -1,25 +1,47 @@
 <script>
 import RestaurantCard from '../components/RestaurantCard.vue';
 import {store} from '../js/store.js';
+import axios from 'axios';
 
 export default {
   name:"Restaurant",
   data() {
     return {
       store,
+      apiUrl:'http://127.0.0.1:8000/api/restaurants',
+      restaurant: null,  // Variável para armazenar o restaurante individual
+
     }
   },
   components: {
     RestaurantCard,
   },
- 
+
+  methods: {
+    getRestaurant (){
+      console.log('Chiamata axios iniziata') 
+      axios.get(`${this.apiUrl}/${this.$route.params.id}`)
+        .then((response) => {
+          if (response.data) {
+            console.log(response.data.results);
+            this.restaurant = response.data.results;  // Assegna i dati ricevuti alla variabile
+          } else {
+            console.error('Nessun piatto trovato nel ristorante');
+          }
+        })
+      },
+    },
+    created(){
+    this.getRestaurant();
+    },
 }
 </script>
 
 <template>
 <p>Restaurant ID: {{ $route.params.id }} </p>
-<h1>Sono un singolo ristorante </h1>
-<RestaurantCard :restaurantObject = 'restaurant' />
+<RestaurantCard :restaurantObject="restaurant" />  
+<h1>{{restaurant.name}}</h1>
+
 </template>
 
 <style scoped lang="scss">
