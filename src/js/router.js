00c0db3e191d2f640e6e9b5f7a82c149ba7e4b29
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-10 21:22:35 +0100

    RF4-changed name error page

--- src/js/router.js
+++ src/js/router.js
@@ -2,12 +2,16 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "../pages/Home.vue";
 import Checkout from "../pages/Checkout.vue";
 import Restaurant from "../pages/Restaurant.vue";
+import ErrorPage from "../pages/ErrorPage.vue";
 
 const routes = [
   //{ path: "/about", component: AboutView },
   { path: '/', component: Home },
   { path: '/checkout', component: Checkout },
-  { path: '/restaurant/:id', component: Restaurant, name: 'singleRestaurant' }
+  { path: '/restaurant/:id', component: Restaurant, name: 'singleRestaurant' },
+  { path: '/errore', component: ErrorPage, name:'ErrorPage' },
+
+
 ];
 
 const router = createRouter({
