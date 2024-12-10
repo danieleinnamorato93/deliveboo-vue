d40debaa62a720ad77cc7b0183417df2ffd2b272
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-10 15:27:15 +0100

    Added badge in restaurant type name

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,35 +1,40 @@
 <script>
 export default {
   data() {
-    return {
-      
-    }
+    return {};
   },
   props: {
     restaurantObject: {
-      type: Object, required: true,
-    }
-
-  }
-}
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
 
 <template>
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../../public/1600w-9Gfim1S8fHg.webp" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{restaurantObject.name}}</h5>
-        <h5 class="card-text">{{restaurantObject.types[0].name}}</h5>
-        <p class="card-text">Descrizione: {{restaurantObject.types[0].description }}</p>
+  <div class="card mb-3" style="max-width: 540px">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img
+          src="../../public/1600w-9Gfim1S8fHg.webp"
+          class="img-fluid rounded-start"
+          alt="..."
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ restaurantObject.name }}</h5>
+          <span class="badge rounded-pill text-bg-danger">
+            <h6 class="card-text">{{ restaurantObject.types[0].name }}</h6>
+          </span>
+          <p class="card-text">
+            Descrizione: {{ restaurantObject.types[0].description }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
