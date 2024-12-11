e681a3ed767a02e1db6569f89626b3dd148bfcff
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-11 18:58:28 +0100

    RF5- created route for payment page

--- src/js/router.js
+++ src/js/router.js
@@ -2,12 +2,15 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "../pages/Home.vue";
 import CartPage from "../pages/CartPage.vue";
 import Restaurant from "../pages/Restaurant.vue";
+import PaymentPage from "../pages/PaymentPage.vue";
 
 const routes = [
   //{ path: "/about", component: AboutView },
   { path: '/', component: Home },
   { path: "/carrello", component: CartPage, name: 'Carrello' },
-  { path: '/restaurant/:id', component: Restaurant, name: 'singleRestaurant' }
+  { path: '/restaurant/:id', component: Restaurant, name: 'singleRestaurant' },
+  { path: '/payment/:orderId', component: PaymentPage, name: 'payment' },
+
 ];
 
 const router = createRouter({
