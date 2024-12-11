7d117fceaad8fa20c62fabb42fd6c1979ff27a06
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-11 20:06:30 +0100

    RF5- testing braintree turorial installing

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -3,6 +3,26 @@ export default {
   data() {
     return {};
   },
+  mounted() {
+    let brainTreeScript = document.createElement('script');
+    brainTreeScript.setAttribute('src', 'https://js.braintreegateway.com/web/dropin/1.43.0/js/dropin.min.js');
+    document.head.appendChild(brainTreeScript);
+    
+    setTimeout(this.setBrainTree, 3000);
+  },
+  methods: {
+    setBrainTree() {
+      //documentazione: https://developer.paypal.com/braintree/docs/start/hello-client/javascript/v3/
+      window.braintree.dropin.create({
+        authorization: "scrofa",
+        container: document.getElementById('dropin-container'),
+      }).then((dropinInstance) => {
+        console.log('Braintree is working', dropinInstance);
+      }).catch((error) => { 
+        console.error('BrainTree is not working properly!',error);
+      });
+    }
+  }
 }
 </script>
 
@@ -10,7 +30,12 @@ export default {
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1 class="text-center">Pagamento per ordine con numero: {{$route.params.orderId}}</h1>
+        <h1 class="text-center">Pagamento per ordine con numero: {{ $route.params.orderId }}</h1>
+      </div>
+      <div class="col-12">
+        <div id="dropin-container">
+
+        </div>
       </div>
     </div>
   </div>
