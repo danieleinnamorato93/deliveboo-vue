c6d3205aea1d5399a1ae3947d79fd45aad7d29e4
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-11 15:31:13 +0100

    Added Responsive Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,100 +3,124 @@ export default {
   data() {
     return {
       isHidden: true,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <template>
-        <nav class="d-flex align-items-center justify-content-between p-3">
-            <div>
-                <img src="../../public/Food-Delivery-Service-PNG-High-Quality-Image.png" alt="logo">
-                <RouterLink to="/" ><h1 class="text-decoration-none">Deliveboo - Food Delivery </h1></RouterLink>
-            </div>
-            <!-- Sezione Link -->
-            <div class="nav-link">
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/checkout">Carrello</RouterLink>
-                <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                  Sei un Ristoratore?
-                </button>
-            </div>
+  <nav class="d-flex align-items-center justify-content-between p-3">
+    <div class="d-flex align-items-center">
+      <img
+        src="../../public/Food-Delivery-Service-PNG-High-Quality-Image.png"
+        alt="logo"
+      />
+      <RouterLink to="/"
+        ><h1 class="text-decoration-none d-none d-md-block ms-2">
+          DeliveBoo
+        </h1></RouterLink
+      >
+    </div>
+    <!-- Sezione Link -->
+    <div class="nav-link">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/checkout">Carrello</RouterLink>
+      <button
+        type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+      >
+        Sei un Ristoratore?
+      </button>
+    </div>
 
-            <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <button type="button" class="log-btn me-2"><a href="http://127.0.0.1:8000/login">Accedi al tuo ristorante</a></button>
-                        <button type="button" class="log-btn me-2"><a href="http://127.0.0.1:8000/register">Registra il tuo ristorante</a></button>
-                        <button type="button" class="close-btn" data-bs-dismiss="modal">Chiudi</button>
-                    </div>
-                </div>
-            </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-body">
+            <button type="button" class="log-btn me-2">
+              <a href="http://127.0.0.1:8000/login">Accedi al tuo ristorante</a>
+            </button>
+            <button type="button" class="log-btn me-2">
+              <a href="http://127.0.0.1:8000/register"
+                >Registra il tuo ristorante</a
+              >
+            </button>
+            <button type="button" class="close-btn" data-bs-dismiss="modal">
+              Chiudi
+            </button>
+          </div>
         </div>
-
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped lang="scss">
 // Stile Navbar
 nav {
-    min-height: 2.5rem;
-    background-color: #4fae5a;
-    a{
-        text-decoration: none;
-    }
-    img {
-        height: 5.5rem;
-        transition: scale 150ms;
-    }
-    img:hover {
-        scale: (1.2);
-    }
-    h1 {
-        margin-left: 20px;
-        display: inline;
-        vertical-align: middle;
-        color: white;
-        font-weight: 700;
-        text-decoration: underline;
-    }
+  min-height: 2.5rem;
+  background-color: #4fae5a;
+  a {
+    text-decoration: none;
+  }
+  img {
+    height: 5.5rem;
+    transition: scale 150ms;
+  }
+  img:hover {
+    scale: (1.2);
+  }
+  h1 {
+    margin-left: 20px;
+    display: inline;
+    vertical-align: middle;
+    color: white;
+    font-weight: 700;
+    text-decoration: underline;
+  }
 }
-
 
 // Sezione Link
 .nav-link {
-    * {
-        margin-right: 20px;
-        text-decoration: none;
-        color: white;
-        font-size: 1.2rem;
-        font-weight: 700;
-    }
+  * {
+    margin-right: 20px;
+    text-decoration: none;
+    color: white;
+    font-size: 1.2rem;
+    font-weight: 700;
+  }
 }
 
 button {
-    border: 1px;
-    padding: 5px;
-    border-radius: 12px;
-    display: inline;
-    border: 3px solid black;
-    background-color: #ea555f;
-    color: black;
+  border: 1px;
+  padding: 5px;
+  border-radius: 12px;
+  display: inline;
+  border: 3px solid black;
+  background-color: #ea555f;
+  color: black;
 }
 
-
 .log-btn {
-    background-color: #4fae5a;
+  background-color: #4fae5a;
 }
 
 .close-btn {
-    background-color: #ea555f;
+  background-color: #ea555f;
 }
 
 a {
-    text-decoration: underline;
-    color: black;
+  text-decoration: underline;
+  color: black;
 }
 </style>
