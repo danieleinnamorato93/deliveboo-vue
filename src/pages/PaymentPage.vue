3ac25b6903ef77bf8a14ff0732a9aea6ff1505dc
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-11 19:22:09 +0100

    RF5- added order id in page

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -7,7 +7,13 @@ export default {
 </script>
 
 <template>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Pagamento per ordine con numero: {{$route.params.orderId}}</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
