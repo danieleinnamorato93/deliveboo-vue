f9814e3765c82f8e371dfb503a2a40520fc66d6c
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-11 14:57:00 +0100

    RF4-added properties to order in my cartPage

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -2,15 +2,22 @@
 export default {
   data() {
     return {
-      
-    }
-  }
+      cart: JSON.parse(localStorage.getItem('cart')) || [],  // Carica il carrello dal localStorage
+      order: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone_number: '',
+        address: '',
+        total: 0,
+        paymentMethod: 'cash'
+      }
+    };
+  },
 }
 </script>
-
 <template>
-<h1>Ciao sono il carrello</h1>
+  <h1>Ciao sono il carrello</h1>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
