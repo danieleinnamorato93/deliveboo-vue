3b5450c8fb149df69a177544f81108519e3dcaf4
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-12 18:53:23 +0100

    basic jumbo & overlay done

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -7,15 +7,31 @@ export default {
 </script>
 
 <template>
-  <div class="jumbo"></div>
+  <div class="jumbo">
+    <div class="overlay"></div>
+  </div>
 </template>
 
 <style scoped>
 .jumbo {
-  min-height: 350px;
-  width: 100%;
+  position: relative;
+  min-height: 540px;
 
+  background-image: url("../assets/imgs/pexels-horizon-content-2100060-3738730.jpg");
   background-position: center;
   background-size: cover;
+}
+.overlay {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    0deg,
+    transparent 0,
+    transparent 65%,
+    rgba(0, 0, 0, 0.5)
+  );
 }
 </style>
