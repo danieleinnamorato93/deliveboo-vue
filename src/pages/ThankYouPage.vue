6948da5125627ee237f9dfbd0c741f21b321bca2
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-13 09:29:13 +0100

    Rf5-created thank you page

---
+++ src/pages/ThankYouPage.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Grazie ell'ordine effettuato! A breve riceverai una email con tutti i dettagli!</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+</style>
