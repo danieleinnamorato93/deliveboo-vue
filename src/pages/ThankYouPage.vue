6e91857ad149c0e90108790bb983360081c99ef1
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-15 22:41:58 +0100

    RF4-fixing typing error

--- src/pages/ThankYouPage.vue
+++ src/pages/ThankYouPage.vue
@@ -7,13 +7,13 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h1>Grazie ell'ordine effettuato! A breve riceverai una email con tutti i dettagli!</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="mt-4">Grazie dell'ordine effettuato.
+          A breve riceverai una email con tutti i dettagli!</h1>
+      </div>
     </div>
+  </div>
 </template>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
