7253060d5f2174e02810372cc76aa0e62450e59d
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-16 17:04:37 +0100

    added background and home button in thankyoupage

--- src/pages/ThankYouPage.vue
+++ src/pages/ThankYouPage.vue
@@ -9,31 +9,58 @@ export default {
 </script>
 
 <template>
-  <div class="container bg-img-container">
-    <div class="row">
+  <div class="flud-container bg-img-container">
+    <div class="row d-flex justify-content-center align-items-center">
       <div class="col-12 text-center">
-        <h1 class="mt-4">
+        <h1 class="mt-4 highlighted-text">
           Grazie dell'ordine effettuato. A breve riceverai una email con tutti i
           dettagli!
         </h1>
-        <h3 class="mt-5">Se hai dubbi, non esitare a contattarci</h3>
-        <RouterLink to="/"
-          ><button class="btn btn-primary">Torna alla home</button></RouterLink
-        >
+        <h3 class="mt-5 highlighted-text">
+          Se hai dubbi, non esitare a contattarci
+        </h3>
+        <RouterLink to="/">
+          <button class="btn btn-primary mt-4">Torna alla home</button>
+        </RouterLink>
       </div>
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 .bg-img-container {
-  background-image: url("");
-  background-size: cover;
+  background-image: url("../../public/freepik__background__2213.png");
+  background-color: #fae7d3;
+  background-size: contain;
   background-position: center;
   background-attachment: fixed;
   min-height: 100vh;
+  display: flex;
   justify-content: center;
   align-items: center;
-  color: red;
   text-align: center;
+
+  h1 {
+    font-size: 3rem;
+  }
+}
+
+.highlighted-text {
+  color: #ffffff;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.9);
+}
+
+button {
+  font-size: 1rem;
+  padding: 12px 30px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+}
+
+button:focus {
+  outline: none;
 }
 </style>
