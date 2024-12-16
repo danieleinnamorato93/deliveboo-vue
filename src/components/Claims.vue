7827719372b5ba31472ca707363dad97a1b162cb
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-16 16:44:35 +0100

    button link to home implemented

--- src/components/Claims.vue
+++ src/components/Claims.vue
@@ -7,21 +7,16 @@ export default {
 </script>
 
 <template>
-  <div
-    class="claim-container text-center d-flex justify-content-center align-items-center"
-  >
+  <div class="claim-container text-center">
     <h1 class="claim display-5 display-md-4 display-lg-3 text-wrap">
       Dalla cucina alla tua tavola, un viaggio di gusto
     </h1>
   </div>
-  <h3 class="call mt-3 fw-bold text-wrap text-center fs-5 fs-md-4 fs-lg-3">
-    Hai fame? Scegli il ristorante perfetto per te e ordina subito!
-  </h3>
 </template>
 
 <style lang="scss" scoped>
 .claim-container {
-  height: 110px;
+  padding: 30px;
   background-color: #fae7d3;
 }
 </style>
--- src/pages/ThankYouPage.vue
+++ src/pages/ThankYouPage.vue
@@ -1,19 +1,39 @@
 <script>
+import { RouterLink } from "vue-router";
+
 export default {
   data() {
     return {};
   },
-}
+};
 </script>
 
 <template>
-  <div class="container">
+  <div class="container bg-img-container">
     <div class="row">
       <div class="col-12 text-center">
-        <h1 class="mt-4">Grazie dell'ordine effettuato.
-          A breve riceverai una email con tutti i dettagli!</h1>
+        <h1 class="mt-4">
+          Grazie dell'ordine effettuato. A breve riceverai una email con tutti i
+          dettagli!
+        </h1>
+        <h3 class="mt-5">Se hai dubbi, non esitare a contattarci</h3>
+        <RouterLink to="/"
+          ><button class="btn btn-primary">Torna alla home</button></RouterLink
+        >
       </div>
     </div>
   </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.bg-img-container {
+  background-image: url("");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  text-align: center;
+}
+</style>
